bd58bd9b888f1c820f98e8b56c608431042c60ac
Shelley <156536161+Shelleeyy@users.noreply.github.com>
2025-02-16 22:21:46 -0500

    media folder + gui

---
+++ cat game/src/main/java/com/example/view/gamegui.java
@@ -0,0 +1,12 @@
+package com.example.view;
+
+import javafx.scene.image.Image;
+import javafx.scene.layout.VBox;
+
+public class gamegui {
+    VBox mainbox = new VBox();
+
+    Image atkBackground = new Image("file:src\\main\\java\\com\\example\\media\\images\\tempBackground.jpg");
+
+
+}
